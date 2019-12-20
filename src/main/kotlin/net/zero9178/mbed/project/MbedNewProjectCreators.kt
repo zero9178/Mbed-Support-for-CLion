59a761360e526a4fd183ad782834fa96599dd8ed
zero9178 <markus.boeck02@gmail.com>
2019-12-20 09:49:02 +0100

    Added more console output for when creating a new mbed project but can't get same console output without emulating a TTY for now

--- src/main/kotlin/net/zero9178/mbed/project/MbedNewProjectCreators.kt
+++ src/main/kotlin/net/zero9178/mbed/project/MbedNewProjectCreators.kt
@@ -1,5 +1,7 @@
 package net.zero9178.mbed.project
 
+import com.intellij.notification.NotificationType
+import com.intellij.notification.Notifications
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.openapi.project.Project
@@ -8,15 +10,17 @@ import com.intellij.testFramework.writeChild
 import com.jetbrains.cidr.cpp.cmake.projectWizard.generators.CLionProjectGenerator
 import com.jetbrains.cidr.cpp.cmake.workspace.CMakeWorkspace
 import icons.MbedIcons
+import net.zero9178.mbed.MbedNotification
 import net.zero9178.mbed.ModalTask
 import net.zero9178.mbed.packages.changeTarget
 import net.zero9178.mbed.packages.changeTargetDialog
 import net.zero9178.mbed.state.MbedState
 import org.apache.commons.exec.CommandLine
 import org.apache.commons.exec.DefaultExecutor
-import org.apache.commons.exec.LogOutputStream
+import org.apache.commons.exec.ExecuteException
 import org.apache.commons.exec.PumpStreamHandler
 import java.io.File
+import java.io.OutputStream
 import javax.swing.Icon
 
 /**
@@ -30,17 +34,38 @@ class MbedNewProjectCreators : CLionProjectGenerator<Any>() {
                 project,
                 "Creating new Mbed os project",
                 {
+                    it.isIndeterminate = true
                     val cli = MbedState.getInstance().cliPath
-                    val cl = CommandLine.parse("$cli new .")
+                    val cl = CommandLine.parse("$cli new -vv .")
                     val exec = DefaultExecutor()
                     exec.workingDirectory = File(virtualFile.path)
-                    exec.streamHandler = PumpStreamHandler(object : LogOutputStream() {
-                        override fun processLine(line: String?, logLevel: Int) {
-                            it.text = line ?: return
+                    var output = ""
+                    exec.streamHandler = PumpStreamHandler(object : OutputStream() {
+                        private var flush = false
+                        override fun write(b: Int) {
+                            output += b.toChar()
+                            if (b.toChar() != '\n') {
+                                if (flush) {
+                                    flush = false
+                                    it.text = ""
+                                }
+                                it.text += b.toChar()
+                            } else {
+                                flush = true
+                            }
                         }
                     })
-                    //TODO: Error handling. execute throws upon non 0 exit code
-                    exec.execute(cl)
+
+                    try {
+                        exec.execute(cl)
+                    } catch (e: ExecuteException) {
+                        Notifications.Bus.notify(
+                            MbedNotification.GROUP_DISPLAY_ID_INFO.createNotification(
+                                "mbed failed with exit code ${e.exitValue}\n Output: $output",
+                                NotificationType.ERROR
+                            )
+                        )
+                    }
                 }) {
                 changeTargetDialog(project)?.let { changeTarget(it, project) }
                 virtualFile.writeChild(
