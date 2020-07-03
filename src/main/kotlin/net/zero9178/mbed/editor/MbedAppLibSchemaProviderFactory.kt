55493f5753df256848b685e425c226e21c2c598b
zero9178 <markus.boeck02@gmail.com>
2020-07-03 21:43:55 +0200

    Added schema for mbed profiles that are automatically added when a json file is added as a profile to the project

--- src/main/kotlin/net/zero9178/mbed/editor/MbedAppLibSchemaProviderFactory.kt
+++ src/main/kotlin/net/zero9178/mbed/editor/MbedAppLibSchemaProviderFactory.kt
@@ -7,5 +7,6 @@ import com.jetbrains.jsonSchema.extension.JsonSchemaProviderFactory
  * Provides json schemas for mbed_app.json and mbed_lib.json
  */
 class MbedAppLibSchemaProviderFactory : JsonSchemaProviderFactory {
-    override fun getProviders(project: Project) = mutableListOf(MbedAppSchemaProvider(), MbedLibSchemaProvider())
+    override fun getProviders(project: Project) =
+        mutableListOf(MbedAppSchemaProvider(), MbedLibSchemaProvider(), MbedProfileSchemaProvider(project))
 }
--- src/main/kotlin/net/zero9178/mbed/editor/MbedAppLibSchemaProviders.kt
+++ src/main/kotlin/net/zero9178/mbed/editor/MbedAppLibSchemaProviders.kt
@@ -1,10 +1,12 @@
 package net.zero9178.mbed.editor
 
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.jetbrains.jsonSchema.extension.JsonSchemaFileProvider
 import com.jetbrains.jsonSchema.extension.JsonSchemaProviderFactory
 import com.jetbrains.jsonSchema.extension.SchemaType
 import com.jetbrains.jsonSchema.impl.JsonSchemaVersion
+import net.zero9178.mbed.state.MbedProjectState
 
 class MbedAppSchemaProvider : JsonSchemaFileProvider {
     override fun getName() = "mbed_app.json Schema"
@@ -31,6 +33,24 @@ class MbedLibSchemaProvider : JsonSchemaFileProvider {
         JsonSchemaProviderFactory.getResourceFile(
             MbedAppLibSchemaProviderFactory::class.java,
             "/schemas/schema_lib.json"
+        )
+
+    override fun getSchemaType() = SchemaType.embeddedSchema
+
+    override fun getSchemaVersion() = JsonSchemaVersion.SCHEMA_6
+}
+
+class MbedProfileSchemaProvider(private val myProject: Project) : JsonSchemaFileProvider {
+    override fun getName() = "Mbed profiles Schema"
+
+    override fun isAvailable(file: VirtualFile) = MbedProjectState.getInstance(myProject).additionalProfiles.any {
+        file.path == it
+    }
+
+    override fun getSchemaFile(): VirtualFile? =
+        JsonSchemaProviderFactory.getResourceFile(
+            MbedAppLibSchemaProviderFactory::class.java,
+            "/schemas/schema_profile.json"
         )
 
     override fun getSchemaType() = SchemaType.embeddedSchema
